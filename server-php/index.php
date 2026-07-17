e2120dc7150ff853a4b4ffa1362216135c3198c8
Your name <your.email@example.com>
2026-07-17 17:55:09 +0530

    Move Dockerfile to repo root

--- server-php/index.php
+++ server-php/index.php
@@ -1,5 +1,14 @@
 <?php
 
+header("Access-Control-Allow-Origin: https://quote-forge-eight.vercel.app");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Cache-Control");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
