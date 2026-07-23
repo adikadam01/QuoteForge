e3c3cd79c782c9dadf3ea9aa23584bce94cc67ad
Your name <your.email@example.com>
2026-07-23 13:21:49 +0530

    fixes in index.php

--- server-php/index.php
+++ server-php/index.php
@@ -12,7 +12,7 @@ if (in_array($origin, $allowedOrigins)) {
 }
 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Cache-Control");
+header("Access-Control-Allow-Headers: Content-Type, Cache-Control, Authorization");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
